adb95b76e0fdd740166c79d2c093bfa478f22b8c
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-02-19 23:27:28 -0300

    refactor: main activity

--- app/src/main/java/br/com/fiap/kotlinpracticefiap/MainActivity.kt
+++ app/src/main/java/br/com/fiap/kotlinpracticefiap/MainActivity.kt
@@ -3,45 +3,164 @@ package br.com.fiap.kotlinpracticefiap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.fiap.kotlinpracticefiap.ui.theme.FiapkotlinTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             FiapkotlinTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CepScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CepScreen() {
+    var cepState by remember { mutableStateOf("") }
+    var ufState by remember { mutableStateOf("") }
+    var cidadeState by remember { mutableStateOf("") }
+    var ruaState by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Card(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(text = "CONSULTA CEP", fontSize = 24.sp, color = Color.Black)
+                Text(
+                    text = "Encontre o seu endereço",
+                    fontSize = 20.sp,
+                    color = Color.Black
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                OutlinedTextField(
+                    value = cepState,
+                    onValueChange = { cepState = it },
+                    modifier = Modifier.fillMaxWidth(),
+                    label = { Text(text = "Qual o CEP procurado?") },
+                    trailingIcon = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = "Pesquisar"
+                            )
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number
+                    )
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(
+                    text = "Não sabe o CEP?",
+                    style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row() {
+                    OutlinedTextField(
+                        value = ufState,
+                        onValueChange = { ufState = it },
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(end = 4.dp),
+                        label = { Text(text = "UF?") },
+                        keyboardOptions = KeyboardOptions(
+                            capitalization = KeyboardCapitalization.Characters,
+                            keyboardType = KeyboardType.Text
+                        )
+                    )
+                    OutlinedTextField(
+                        value = cidadeState,
+                        onValueChange = { cidadeState = it },
+                        modifier = Modifier.weight(2f),
+                        label = { Text(text = "Qual a cidade?") },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Text,
+                            capitalization = KeyboardCapitalization.Words
+                        )
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    OutlinedTextField(
+                        value = ruaState,
+                        onValueChange = { ruaState = it },
+                        modifier = Modifier.weight(2f),
+                        label = { Text(text = "O que lembra do nome da rua?") },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Text,
+                            capitalization = KeyboardCapitalization.Words
+                        )
+                    )
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Default.Search, contentDescription = "Pesquisar Rua")
+                    }
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(120) {
+                CardEndereco()
+            }
+        }
+    }
+}
+
+@Composable
+fun CardEndereco() {
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(bottom = 4.dp)
+    ) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+        ) {
+            Text(text = "CEP:")
+            Text(text = "Rua:")
+            Text(text = "Cidade:")
+            Text(text = "Bairro:")
+            Text(text = "UF:")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     FiapkotlinTheme {
-        Greeting("Android")
+        CepScreen()
     }
 }
