01d9ec8674b0ecdc2f3abb4667a441c9009db794
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-02-20 09:56:35 -0300

    refactor: service

--- app/src/main/java/br/com/fiap/kotlinpracticefiap/service/CepService.kt
+++ app/src/main/java/br/com/fiap/kotlinpracticefiap/service/CepService.kt
@@ -7,15 +7,13 @@ import retrofit2.Call
 
 interface CepService {
 
-    //https://viacep.com.br/ws/01001000/json
     @GET("{cep}/json/")
     fun getEndereco(@Path("cep") cep: String): Call<Endereco>
 
-    //https://viacep.com.br/ws/RS/Porto%20Alegre/Domingos/json/
     @GET("{uf}/{cidade}/{rua}/json/")
     fun getEnderecos(
         @Path("uf") uf: String,
         @Path("cidade") cidade: String,
         @Path("rua") rua: String
-    ): List<Call<Endereco>>
+    ): Call<List<Endereco>>
 }
